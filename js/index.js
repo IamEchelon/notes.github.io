b4718aadb9a93d71aad3d62bb26a9baeac732348
Brent Brinkley <brentbrinkley@gmail.com>
2017-06-09 17:11:22 -0400

    minor update

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var synth = new Tone.AMSynth().toMaster()
+
+var elmApp = Elm.Main.fullscreen();
+
+elmApp.ports.signal.subscribe(function(tone_val){
+    if (tone_val) {
+        synth.triggerAttack(tone_val);
+        console.log(tone_val)
+    } else {
+        synth.triggerRelease();
+    }
+
+});
